b17fefc69cbd1eb5f39bd87dcf7bfea7bbf5bd64
Harisanth P S <83811783+harisanth123@users.noreply.github.com>
2022-04-05 14:46:43 +0530

    Initial commit

---
+++ lib/inputpage.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+class inputpage extends StatefulWidget {
+  const inputpage({Key? key}) : super(key: key);
+
+  @override
+  State<inputpage> createState() => _inputpageState();
+}
+
+class _inputpageState extends State<inputpage> {
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+
+  double? _result;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('calculator')),
+          centerTitle: true,
+          backgroundColor: Colors.pinkAccent,
+        ),
+        body: SafeArea(
+
+          child: Center(
+            child: Column(
+              children: [
+                    SizedBox(height: 20),
+                    TextField(
+                      controller: _heightController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText:'height in cm',
+                        icon: Icon(Icons.trending_up)
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      controller: _weightController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: 'weight in kg',
+                        icon: Icon(Icons.line_weight)
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    ElevatedButton(onPressed: (){}, child: Text(
+                      "calculate",
+                      style: TextStyle(color: Colors.white),
+                    ),),
+
+
+
+                    SizedBox(height: 15),
+                    Text(
+                      "_result",
+                      style: TextStyle(
+                        color: Colors.redAccent,
+                        fontSize: 19.4,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    )
+              ],
+            ),
+
+            ),
+          ),
+
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:bmicalculator/inputpage.dart';
+import 'package:flutter/material.dart';
+void main(){
+  runApp(const inputpage());
+}
